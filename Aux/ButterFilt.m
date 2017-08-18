83b49c3f5e9adb7c27f7f522537e7c7a7d15df8e
asoroosh <srafyouni@gmail.com>
2017-08-18 16:00:18 +0100

    housekeeping

--- Aux/ButterFilt.m
+++ Aux/ButterFilt.m
@@ -1,29 +1,31 @@
-function Y=ButterFilt(Y,Ord,bnd,TR)
+function Y=ButterFilt(Y,Ord,bnd_freq,TR)
 % Butterworth filtering of the multi-dim data
+% Input Y should be in IxT form. 
 %
 % SA, NISOx.org, 2017
 %
-if size(Y,1)<size(Y,2)
-    error('The diminsions should be as IxT.')
-end
 
-if isempty(Ord); Ord=4;         end;
-if isempty(bnd); bnd=[.01 .1];  end;
+% if size(Y,1)<size(Y,2)
+%     error('The diminsions should be as IxT.')
+% end
+
+if isempty(Ord); Ord=4;         end; %just to be safe!
+if isempty(bnd_freq); bnd_freq=[.01 .1];  end;
 
 srate = 1/TR;
-bnd   = bnd./(srate/2)
+bnd_radsmpl   = bnd_freq./(srate/2);
 
-if  bnd(1)<0 && numel(bnd)==2
-    disp(['-Highpass filter.'])
-    [b,a] = butter(Ord,bnd(2),'high');
+if  bnd_radsmpl(1)<0 && numel(bnd_radsmpl)==2
+    disp(['-Highpass filter (Butterworth) of order ' num2str(Ord) ' & cutoff: ' num2str(bnd_freq(2)) ' Hz'])
+    [b,a] = butter(Ord,bnd_radsmpl(2),'high');
     Y     = filter(b,a,Y,[],2);
-elseif bnd(2)<0 && numel(bnd)==2
-    disp(['-Lowpass filter.'])
-    [b,a] = butter(Ord,bnd(1),'low');
+elseif bnd_radsmpl(2)<0 && numel(bnd_radsmpl)==2
+    disp(['-Lowepass filter (Butterworth) of order ' num2str(Ord) ' & cutoff: ' num2str(bnd_freq(1)) ' Hz'])
+    [b,a] = butter(Ord,bnd_radsmpl(1),'low');
     Y     = filter(b,a,Y,[],2);    
-elseif and(bnd(1)>0,bnd(2)>0) && diff(bnd)>0 && numel(bnd)==2
-    disp(['-Bandpass filter.'])
-    [b,a] = butter(Ord,bnd);
+elseif and(bnd_radsmpl(1)>0,bnd_radsmpl(2)>0) && diff(bnd_radsmpl)>0 && numel(bnd_radsmpl)==2
+    disp(['-Bandpass filter (Butterworth) of order ' num2str(Ord) ' & band : [' num2str(bnd_freq(1)) ' ' num2str(bnd_freq(2)) '] Hz'])
+    [b,a] = butter(Ord,bnd_radsmpl);
     Y     = filter(b,a,Y,[],2);     
 else
     error('The band is missing or maldefined!')
