40974672cd8bdf450270dd8b3f481fca765e71c3
asoroosh <srafyouni@gmail.com>
2019-01-17 20:46:11 +0000

    Now fixed and ready

--- Python/DSEnDVAR_Demo.py
+++ Python/DSEnDVAR_Demo.py
@@ -8,26 +8,33 @@ University of Oxford, 2019
 """
 
 from DSE import DSE_Calc, DVARS_Calc, CleanNIFTI
-
+import numpy as np
 
 V = '/Users/sorooshafyouni/Desktop/HCP/135932/135932_RS/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_hp2000_clean.nii.gz'
-
-#+++++++++++++++++++CLEAN THE NIFTI+++
-
+#
+##++++++++++++++++++++++++++++++++++++
+##+++++++++++++++++++CLEAN THE NIFTI+++
+#
 Vout = CleanNIFTI(V,scl = 1/100)
+#
+YY = Vout[0]
 
-#+++++++++++++++++++DSE+++
+#YY = np.random.randn(1500,500)
 
-DSEOut  = DSE_Calc(Vout[0],\
+##+++++++++++++++++++++++++++
+##+++++++++++++++++++DSE+++++
+DSEOut  = DSE_Calc(YY,\
         scl        = 0,\
-        demean     = False,\
+        norm       = 0,\
         DSEImage   = False,\
         verbose    = True)
 
-#+++++++++++++++++++DVARS+++
-DVARSOut = DVARS_Calc(Vout[0],\
-               dd=1, \
-               WhichExpVal = 'median', WhichVar = 'hIQRd',\
-               scl = 0,\
-               demean = False,\
+#++++++++++++++++++++++++++++
+#+++++++++++++++++++DVARS++++
+DVARSOut = DVARS_Calc(YY,\
+               dd = 1, \
+               WhichExpVal  = 'median',\
+               WhichVar     = 'hIQRd',\
+               norm = 0,\
+               scl  = 0,\
                DeltapDvarThr = 5)
