69294a44ab841d6c424209615ad11e7fa550ff44
asoroosh <srafyouni@gmail.com>
2019-03-31 12:11:43 +0100

    Demo for python updated

--- Python/DSEnDVAR_Demo.py
+++ Python/DSEnDVAR_Demo.py
@@ -25,43 +25,51 @@ def Save2Pickle(Path2File,Vars):
 #
 print('========Read and clean...')
 
-Vout = CleanNIFTI(RS_fMRI_Dir,scl = 1/100)
-#
-GS = Vout[6] # this is the global signal 
-YY = Vout[0] # This is the cleaned version of the image. 
-
-del Vout
-
-# Just to test everything is sound with the DSE and DAVRS code. 
-#YY = np.random.randn(1500,500)
-
-
-
 print('========Running DSE...')
 ##+++++++++++++++++++DSE+++++
-DSEOut  = DSE_Calc( YY,\
+DSEOut  = DSE_Calc( RS_fMRI_Dir,\
         scl         = 0,\
         norm        = 0,\
         DSEImage    = False,\
         verbose     = False)
 
-
-
-
-print('========Running DVARS...')
-#+++++++++++++++++++DVARS++++
-DVARSOut = DVARS_Calc(YY,\
-               dd            = 1, \
-               WhichExpVal   = 'median',\
-               WhichVar      = 'hIQRd',\
-               norm          = 0,\
-               scl           = 0,\
-               DeltapDvarThr = 5)
-
-
-#++++Write me on a pickle!++++++++++
-Save2Pickle(DSEResults_Dir + 'GS_Demo.pkl',GS)
-Save2Pickle(DSEResults_Dir + 'DSE_Demo.pkl',DSEOut)
-Save2Pickle(DSEResults_Dir + 'DVARS_Demo.pkl',DVARSOut)
-
-print('DONE!')
+#Vout = CleanNIFTI(RS_fMRI_Dir,scl = 1/100)
+##
+#GS = Vout[6] # this is the global signal 
+#YY = Vout[0] # This is the cleaned version of the image. 
+#
+#del Vout
+#
+## Just to test everything is sound with the DSE and DAVRS code. 
+##YY = np.random.randn(1500,500)
+#
+#
+#
+#print('========Running DSE...')
+###+++++++++++++++++++DSE+++++
+#DSEOut  = DSE_Calc( YY,\
+#        scl         = 0,\
+#        norm        = 0,\
+#        DSEImage    = False,\
+#        verbose     = False)
+#
+#
+#
+#
+#print('========Running DVARS...')
+##+++++++++++++++++++DVARS++++
+#DVARSOut = DVARS_Calc(YY,\
+#               dd            = 1, \
+#               WhichExpVal   = 'median',\
+#               WhichVar      = 'hIQRd',\
+#               norm          = 0,\
+#               scl           = 0,\
+#               DeltapDvarThr = 5)
+#
+#
+##++++Write me on a pickle!++++++++++
+#Save2Pickle(DSEResults_Dir + 'GS_Demo.pkl',GS)
+#Save2Pickle(DSEResults_Dir + 'DSE_Demo.pkl',DSEOut)
+#Save2Pickle(DSEResults_Dir + 'DVARS_Demo.pkl',DVARSOut)
+#
+#print('DONE!')
--- Python/Demo/MatlabTest/Mattests.m
+++ Python/Demo/MatlabTest/Mattests.m
@@ -2,9 +2,11 @@ clear
 
 V = '/Users/sorooshafyouni/Desktop/HCP/135932/135932_RS/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_hp2000_clean.nii.gz';
 
-Y = CleanNIFTI(V,'scale',1/100,'demean'); 
+[DSE,DSE_Stats] = DSEvars(V);
 
-[DSE,DSE_Stats] = DSEvars(Y);
-
-[DVARS,DVARS_Stats] = DVARSCalc(Y);
+%Y = CleanNIFTI(V,'scale',1/100,'demean'); 
+%
+% [DSE,DSE_Stats] = DSEvars(Y);
+% 
+% [DVARS,DVARS_Stats] = DVARSCalc(Y);
 
